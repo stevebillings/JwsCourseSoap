ce4bedf8df0de9bf6a8d39726281de126139cd17
sbillings <sbillings@blackducksoftware.com>
2016-05-07 15:47:47 -0400

    placeOrder works

--- src/com/uciext/ws/hw3/service/InventoryWS.java
+++ src/com/uciext/ws/hw3/service/InventoryWS.java
@@ -1,14 +1,22 @@
 package com.uciext.ws.hw3.service;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebParam.Mode;
 import javax.jws.WebService;
 
 import com.uciext.ws.hw3.service.model.catalog.Catalog;
+import com.uciext.ws.hw3.service.model.order.Order;
+import com.uciext.ws.hw3.service.model.orderConfirm.OrderConfirm;
 
-@WebService(targetNamespace = "http://uciext.ws.class3/wsdl")
+@WebService(targetNamespace = "http://uciext.ws.hw3/wsdl")
 public interface InventoryWS {
 	@WebMethod
 	public String hello();
 
 	public Catalog getCatalog();
+
+	public OrderConfirm placeOrder(
+			@WebParam(name = "order", mode = Mode.IN, targetNamespace = "http://uciext.ws.hw3/wsdl") Order order)
+					throws Exception;
 }
--- src/com/uciext/ws/hw3/service/impl/InventoryWSImpl.java
+++ src/com/uciext/ws/hw3/service/impl/InventoryWSImpl.java
@@ -1,14 +1,24 @@
 package com.uciext.ws.hw3.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.jws.WebService;
 
 import com.uciext.ws.hw3.backend.InventoryManager;
 import com.uciext.ws.hw3.backend.impl.InventoryManagerImpl;
 import com.uciext.ws.hw3.backend.model.CatalogDAO;
+import com.uciext.ws.hw3.backend.model.OrderConfirmDAO;
+import com.uciext.ws.hw3.backend.model.OrderDAO;
 import com.uciext.ws.hw3.backend.model.ProductDAO;
+import com.uciext.ws.hw3.backend.model.ProductOrderDAO;
 import com.uciext.ws.hw3.service.InventoryWS;
 import com.uciext.ws.hw3.service.model.catalog.Catalog;
 import com.uciext.ws.hw3.service.model.catalog.Product;
+import com.uciext.ws.hw3.service.model.order.Order;
+import com.uciext.ws.hw3.service.model.order.ProductOrder;
+import com.uciext.ws.hw3.service.model.orderConfirm.OrderConfirm;
 import com.uciext.ws.hw3.util.Util;
 
 @WebService(portName = "InventoryPort", serviceName = "InventoryService", targetNamespace = "http://uciext.ws.hw3/wsdl", endpointInterface = "com.uciext.ws.hw3.service.InventoryWS")
@@ -45,6 +55,51 @@ public class InventoryWSImpl implements InventoryWS {
 
 		Util.log("SOAP getCatalog response [product count=" + catalog.getProduct().size());
 		return catalog;
+	}
+
+	@Override
+	public OrderConfirm placeOrder(Order order) throws Exception {
+		Util.log("SOAP placeOrder request order=" + order);
+
+		// Prepare order
+		OrderDAO orderDao = new OrderDAO();
+		Long orderId = (new Date()).getTime();
+		orderDao.setOrderId(orderId);
+		Util.log("order.getOrderNumber(): " + order.getOrderNumber()); // TODO
+		orderDao.setOrderNumber(order.getOrderNumber());
+		orderDao.setVendorCode(order.getVendorCode());
+		orderDao.setVendorName(order.getVendorName());
+
+		// Product list
+		List<ProductOrderDAO> productDaoList = new ArrayList<>();
+		for (ProductOrder productOrder : order.getProductOrder()) {
+			// Product fields
+			ProductDAO productDao = new ProductDAO();
+			productDao.setProductName(productOrder.getProductName());
+			productDao.setSku(productOrder.getProductSku());
+
+			// ProductOrder
+			ProductOrderDAO productOrderDao = new ProductOrderDAO();
+			productOrderDao.setOrderQuantity(productOrder.getOrderQuantity());
+			productOrderDao.setProduct(productDao);
+
+			productDaoList.add(productOrderDao);
+		}
+		orderDao.setProductOrderList(productDaoList);
+
+		// Process order
+		OrderConfirmDAO orderConfirmDao = manager.processOrder(orderDao);
+
+		// Create a status response
+		OrderConfirm orderConfirm = new OrderConfirm();
+		orderConfirm.setConfirmNumber(String.valueOf(orderConfirmDao.getOrderConfirmId()));
+		orderConfirm.setOrderNumber(orderConfirmDao.getOrderNumber());
+		orderConfirm.setTotalOrderPrice(orderConfirmDao.getTotalOrderPrice());
+		orderConfirm.setVendorCode(orderConfirmDao.getVendorCode());
+
+		Util.log("SOAP orderConfirm response [confirmNumber=" + orderConfirm.getConfirmNumber() + "]");
+
+		return orderConfirm;
 	}
 
 }
